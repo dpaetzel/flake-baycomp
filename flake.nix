ade931780591a029a97d6b288fac26b1be5685d7
David Pätzel <david.paetzel@uni-a.de>
2022-01-25 11:49:07 +0100

    Pin nixpkgs, pin Python version

--- flake.nix
+++ flake.nix
@@ -1,19 +1,20 @@
 {
   description = "baycomp";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  # 2022-01-24
+  inputs.nixpkgs.url =
+    "github:NixOS/nixpkgs/8ca77a63599ed951d6a2d244c1d62092776a3fe1";
   inputs.src = {
     url = "github:janezd/baycomp";
     flake = false;
   };
 
-  outputs = { self, nixpkgs, src }: rec {
+  outputs = { self, nixpkgs, src }:
+    with import nixpkgs { system = "x86_64-linux"; };
+    let python = python39;
+    in rec {
 
-    defaultPackage.x86_64-linux = packages.x86_64-linux.baycomp;
-
-    packages.x86_64-linux.baycomp =
-      with import nixpkgs { system = "x86_64-linux"; };
-      python3.pkgs.buildPythonPackage rec {
+      defaultPackage.x86_64-linux = python.pkgs.buildPythonPackage rec {
         pname = "baycomp";
         version = "unstable";
 
@@ -21,7 +22,7 @@
 
         doCheck = false;
 
-        propagatedBuildInputs = with python3.pkgs; [ matplotlib numpy scipy ];
+        propagatedBuildInputs = with python.pkgs; [ matplotlib numpy scipy ];
 
         meta = with lib; {
           homepage = "https://github.com/janezd/baycomp";
@@ -29,5 +30,5 @@
           license = licenses.mit;
         };
       };
-  };
+    };
 }
